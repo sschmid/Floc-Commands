48e1df9143b375bd1cb2ced5eecd3d03ec81f947
Simon Schmid <mail@sschmid.com>
2013-06-07 18:26:37 +0200

    Initial Commit

---
+++ Floc-Commands/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Floc-Commands
+//
+//  Created by Simon Schmid on 07.06.13.
+//  Copyright (c) 2013 Simon Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Floc-CommandsTests/Floc_CommandsTests.m
@@ -0,0 +1,32 @@
+//
+//  Floc_CommandsTests.m
+//  Floc-CommandsTests
+//
+//  Created by Simon Schmid on 07.06.13.
+//  Copyright (c) 2013 Simon Schmid. All rights reserved.
+//
+
+#import "Floc_CommandsTests.h"
+
+@implementation Floc_CommandsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Floc-CommandsTests");
+}
+
+@end
