76991911d48fdba45ebf6e1ca13632edb4bbfdd9
Simon Schmid <mail@sschmid.com>
2013-06-30 17:08:50 +0200

    New icons.

--- Floc-Commands/AppDelegate.m
+++ Floc-Commands/AppDelegate.m
@@ -10,23 +10,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
-    [self addIcon];
-
     self.window.backgroundColor = [UIColor blackColor];
+
+    [self showIcon];
+
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)addIcon {
+- (void)showIcon {
     UIImage *image = [UIImage imageNamed:@"Floc-Commands-144.png"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-    CGRect centerFrame = CGRectMake(self.window.center.x - imageView.frame.size.width / 2,
-            self.window.center.y - imageView.frame.size.height / 2,
-            imageView.frame.size.width,
-            imageView.frame.size.height);
-
-    imageView.frame = centerFrame;
+    imageView.center = self.window.center;
     [self.window addSubview:imageView];
 }
 
